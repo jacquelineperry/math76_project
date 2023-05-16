6487eeba43ca099b20667216f90fb5b27848a0cd
cooperwharrison <cooper.w.harrison.22@dartmouth.edu>
2023-05-16 15:45:22 -0400

    Updated SIR.py

--- src/stocks_network/sir.py
+++ src/stocks_network/sir.py
@@ -1,86 +1,39 @@
 #%%
-import numpy as np
-import random
-import matplotlib.pyplot as plt
-from correlation import Correlation
+import networkx as nx
+import ndlib.models.ModelConfig as mc
+import ndlib.models.epidemics as ep
+from network_graph import NetworkGraph
 import pandas as pd
-
-
-def simulate_sir(adjacency_matrix, infected_node, recovery_rate, num_iterations):
-    num_nodes = adjacency_matrix.shape[0]
-    
-    # Initialize the state of each node (S: Susceptible, I: Infected, R: Recovered)
-    states = np.zeros(num_nodes, dtype=int)
-    states[infected_node] = 1  # Set the initially infected node
-    
-    infected_count = [1]  # Track the number of infected nodes in each iteration
-    
-    for _ in range(num_iterations):
-        new_states = states.copy()
-        
-        # Iterate over each node
-        for node in range(num_nodes):
-            if states[node] == 1:  # If the node is infected
-                neighbors = np.nonzero(adjacency_matrix[node])[0]  # Find neighboring nodes
-                print(neighbors)
-                # Iterate over each neighboring node
-                for neighbor in neighbors:
-                    if states[neighbor] == 0:  # If the neighbor is susceptible
-                        # Calculate the probability of transmission based on correlation
-                        transmission_prob = adjacency_matrix[node][neighbor]
-                        
-                        # Check if transmission occurs
-                        if random.random() < transmission_prob:
-                            new_states[neighbor] = 1  # Set the neighbor as infected
-        
-                # Check if recovery occurs
-                if random.random() < recovery_rate:
-                    new_states[node] = 2  # Set the node as recovered
-        
-        # Update the states after each iteration
-        states = new_states.copy()
-        infected_count.append(np.count_nonzero(states == 1))
-    
-    return infected_count
+from bokeh.io import output_notebook, show
+from ndlib.viz.bokeh.DiffusionTrend import DiffusionTrend
 
 #%%
-
-# Example usage
-adjacency_matrix = np.array([[0, 0.7, 0.5, 0, 0],
-                             [0.7, 0, 0.3, 0, 0.9],
-                             [0.5, 0.3, 0, 0.8, 0],
-                             [0, 0, 0.8, 0, 0.4],
-                             [0, 0.9, 0, 0.4, 0]])
-
-infected_node = 0  # Choose the starting infected node
-recovery_rate = 0.1
-num_iterations = 20
-
-infected_count = simulate_sir(adjacency_matrix, infected_node, recovery_rate, num_iterations)
-
-# Plotting the number of infected nodes over iterations
-plt.plot(range(num_iterations + 1), infected_count)
-plt.xlabel('Iterations')
-plt.ylabel('Number of Infected Nodes')
-plt.title('Agent-based SIR Model Simulation')
-plt.show()
-
-
-#%%
+# Network topology
 hist_data_weekly = pd.read_csv(r'C:\Users\coope\Documents\Math 76\math76_project\data\historical_prices.csv')
-corr = Correlation(hist_data_weekly)
-matrix = corr.get_corr_matrix("pearsonr")
-print(matrix)
+g = NetworkGraph(hist_data_weekly).create_basic_network("pearsonr")
+
+#%%
+
+# Model selection
+model = ep.SIRModel(g)
+
+# Model Configuration
+cfg = mc.Configuration()
+# setting infection probability
+cfg.add_model_parameter('beta', 0.01)
+# setting removal probability
+cfg.add_model_parameter('gamma', 0.005)
+cfg.add_model_parameter("fraction_infected", 0.05)
+model.set_initial_status(cfg)
+
+# Simulation execution
+iterations = model.iteration_bunch(200)
+trends = model.build_trends(iterations)
+
 # %%
 
-infected_node = 0  # Choose the starting infected node
-recovery_rate = 0.1
-num_iterations = 20
-infected_count = simulate_sir(matrix, infected_node, recovery_rate, num_iterations)
+# visualize the simulaton
+viz = DiffusionTrend(model, trends)
+p = viz.plot(width=400, height=400)
+show(p)
 
-plt.plot(range(num_iterations + 1), infected_count)
-plt.xlabel('Iterations')
-plt.ylabel('Number of Infected Nodes')
-plt.title('Agent-based SIR Model Simulation')
-plt.show()
-# %%
